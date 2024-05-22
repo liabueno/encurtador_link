8409ccb707d51c6e6bee41c0acfe22a42ec0940b
liabueno <juliavbueno13@gmail.com>
2024-05-22 18:18:08 -0300

    .

--- resources/views/site/recover.blade.php
+++ resources/views/site/recover.blade.php
@@ -10,7 +10,7 @@
                 <img src=" {{ asset('img/recover.png') }} " class="img-thumbnail capa" alt="">
             </div>
             <div class="col-6 justify-content-center align-items-center">
-                <h1 class="text-center">Recuperar Senha</h1>
+                <h1 class="text-center recover">Recuperar Senha</h1>
                 @component('site.layouts._components.form_recover')
                 @endcomponent
             </div>
